180f400b7ee6fc47d6a29bbd6c89b5cd3942ef7c
lpraat <pratissolil@gmail.com>
2018-10-28 00:51:06 +0200

    Other hyperparameter tuner improvements - used a class instead of a function - hyperparameters are automatically parsed from the callable

--- src/tuning/tune.py
+++ src/tuning/tune.py
@@ -1,7 +1,13 @@
 from src.alg import Ensemble, ItemKNN
-from src.tuning.tuner import Hyperparameter, Callable, hyperparameter_search
+from src.tuning.tuner import Hyperparameter, Callable, HyperparameterTuner
 
-alpha = Hyperparameter("alpha", [0.1])
+
+# TODO this like src/run.py should go in the .gitignore
+# Usage example
+# I want to tune alpha and h for the following model
+# Ensemble((ItemKNN(("artist_set", 0.1, {}), ("album_set", 0.2, {}), alpha=0.56, h=2.5), 1))
+
+alpha = Hyperparameter("alpha", [0.56])
 h = Hyperparameter("h", [2.5])
 
 ensemble = Callable(
@@ -11,12 +17,10 @@ ensemble = Callable(
             obj=ItemKNN,
             args=[("artist_set", 0.1, {}), ("album_set", 0.2, {})],
             kwargs={"alpha": alpha, "h": h}
-        ), 0.4)
+        ), 1)
     ],
     kwargs=None
 )
 
-# TODO this parameter can be removed, since tuner can build it by looking at the model
-hyperparameters = [alpha, h]
-
-hyperparameter_search(ensemble, hyperparameters, write_to_file=True, cartesian_product=True)
+tuner = HyperparameterTuner(ensemble, cartesian_product=True)
+tuner.run()
--- src/tuning/tuner.py
+++ src/tuning/tuner.py
@@ -1,16 +1,26 @@
+import pprint
 from random import shuffle
 
 import numpy as np
-from timeit import default_timer as timer
-import itertools
-import pprint
 import os
+from timeit import default_timer as timer
 
-# TODO add some docs
 from src.const import tune_path
+import itertools
 
 
 class Callable:
+    """A callable model. Either a function or a class.
+
+    Attributes
+    ----------
+    obj : function or class
+        Callable model.
+    args : list
+        Callable's arguments.
+    kwargs : dict
+        Callable's named arguments.
+    """
     def __init__(self, obj, args=None, kwargs=None):
         self.obj = obj
         self.args = args
@@ -18,176 +28,254 @@ class Callable:
 
 
 class Hyperparameter:
+    """Hyperparameter to be tuned.
+
+    Attributes
+    ----------
+    name : str
+        Identifier used to represent the hyperparameter.
+    interval : list
+        Hyperparameter search space.
+    """
     def __init__(self, name, interval):
         self.name = name
         self.interval = interval
         self.value = None
 
     def random_sample(self):
+        """
+        Randomly sample and set a value from the search space.
+        """
         self.value = np.random.choice(self.interval)
 
 
-def hyperparameter_search(callable_model, hyperparameters, cartesian_product=False, write_to_file=True,
-                          max_resample=1000):
+class HyperparameterTuner:
+    """Tuner that evaluates a model in a given hyperparameter space.
+
+    Attributes
+    ----------
+    callable_model : Callable
+        Callable of the model to be tuned.
+    cartesian_product : bool
+        If True search will be done by considering each possible combination of hyperparameters values.
+        Otherwise search is performed by sampling randomly in hyperparameters search space.
+    cartesian_products : list
+        All the possible combinations of hyperparameter values when considering cartesian products.
+    max_resample : function or class
+        Maximum number of resampling performed in a sampling iteration from the search space when not
+        considering cartesian products.
+    resample_try : int
+        Number of resample done in a sampling iteration from the search space when not considering cartesian
+        products.
+    file_name : str
+        File name where the results will be written.
+    hyperparameters : list
+        Hyperparameters to be tuned.
+    parse_hyperparam : bool
+        True if the hyperparameters have been already extracted from the callable, False otherwise.
+    history : dict
+        Container to store the evaluation results.
+
+    """
+    def __init__(self, callable_model, cartesian_product=False, write_to_file=True, max_resample=1000):
+        """Main constructor
+
+        Parameters
+        ---------------
+        callable_model : function or class
+            Callable of the model to be tuned.
+        cartesian_product : bool
+            Whether to consider the cartesian products of the hyperparameter values or not.
+        write_to_file : bool
+            Whether to write on file or not the tuning results.
+        max_resample : integer
+            Number of resample done in a sampling iteration from the search space when not considering cartesian
+            products.
+        """
+        self.callable_model = callable_model
+        self.cartesian_product = cartesian_product
+        self.max_resample = max_resample
+
+        self.hyperparameters = []
+
+        self.parse_hyperparam = True
+        self.parse_callable(callable_model)
+        self.parse_hyperparam = False
+
+        assert len(self.hyperparameters) > 0, "You must specify the hyperparameters to search for"
+
+        self.cartesian_products = None
+        if self.cartesian_product:
+
+            if len(self.hyperparameters) == 1:
+                print("Cartesian product search needs atleast 2 hyperparameters!")
+                return
+
+            self.cartesian_products = list(itertools.product(*[h.interval for h in self.hyperparameters]))
+            shuffle(self.cartesian_products)
+
+        else:
+            self.resample_try = 0
+
+        # Create file to write if write_to_file is set
+        self.file_name = None
+        if write_to_file:
+            self.create_history_file()
+
+        # Dict to store the results obtained for different combination for hyperparameters
+        # Key -> Tuple(h1, h2, ..., hn) where hi is the ith hyperparameter value -> MAP@K
+        self.history = dict()
+
+
+    @staticmethod
     def is_callable(entity):
         return isinstance(entity, Callable)
 
+    @staticmethod
     def is_sequence(obj):
         return isinstance(obj, list) or isinstance(obj, tuple) or isinstance(obj, dict)
 
-    def check_hyperparameter(v):
-        if isinstance(v, Hyperparameter):
-            return v.value
+    def check_hyperparameter(self, el):
+        if isinstance(el, Hyperparameter):
+            if self.parse_hyperparam:
+                self.hyperparameters.append(el)
+            else:
+                return el.value
         else:
-            return v
+            return el
 
-    def parse_sequence(seq):
+    def parse_callable(self, callable_thing):
+        args = []
+        if callable_thing.args:
+            for arg in callable_thing.args:
+
+                if self.is_callable(arg):
+                    args.append(self.parse_callable(arg))
+                if self.is_sequence(arg):
+                    args.append(self.parse_sequence(arg))
+                else:
+                    args.append(self.check_hyperparameter(arg))
+
+        kwargs = dict()
+        if callable_thing.kwargs:
+            for k, v in callable_thing.kwargs.items():
+
+                if self.is_callable(v):
+                    kwargs.update({k: self.parse_callable(v)})
+                elif self.is_sequence(v):
+                    kwargs.update({k: self.parse_sequence(v)})
+                else:
+                    kwargs.update({k: self.check_hyperparameter(v)})
+
+        return callable_thing.obj(*args, **kwargs)
+
+    def parse_sequence(self, seq):
         if isinstance(seq, list) or isinstance(seq, tuple):
 
             # Treat both list and tuples as lists
             new_seq = []
             for i, el in enumerate(seq):
-                if is_callable(el):
-                    new_seq.append(parse_callable(el))
-                elif is_sequence(el):
+                if self.is_callable(el):
+                    new_seq.append(self.parse_callable(el))
+                elif self.is_sequence(el):
 
-                    new_seq.append(parse_sequence(el))
+                    new_seq.append(self.parse_sequence(el))
                 else:
-                    new_seq.append(check_hyperparameter(el))
+                    new_seq.append(self.check_hyperparameter(el))
 
             return new_seq
         else:  # it is a dict
             new_d = dict()
 
             for k, v in seq.items():
-                if is_callable(v):
-                    new_d.update({k: parse_callable(v)})
-                elif is_sequence(v):
-                    new_d.update({k: parse_callable(v)})
+                if self.is_callable(v):
+                    new_d.update({k: self.parse_callable(v)})
+                elif self.is_sequence(v):
+                    new_d.update({k: self.parse_callable(v)})
                 else:
-                    new_d.update({k: check_hyperparameter(v)})
+                    new_d.update({k: self.check_hyperparameter(v)})
 
             return new_d
 
-    def parse_callable(callable_thing):
-
-        args = []
-        if callable_thing.args:
-            for arg in callable_thing.args:
-
-                if is_callable(arg):
-                    args.append(parse_callable(arg))
-                if is_sequence(arg):
-                    args.append(parse_sequence(arg))
-                else:
-                    args.append(check_hyperparameter(arg))
-
-        kwargs = dict()
-        if callable_thing.kwargs:
-            for k, v in callable_thing.kwargs.items():
-
-                if is_callable(v):
-                    kwargs.update({k: parse_callable(v)})
-                elif is_sequence(v):
-                    kwargs.update({k: parse_sequence(v)})
-                else:
-                    kwargs.update({k: check_hyperparameter(v)})
-
-        return callable_thing.obj(*args, **kwargs)
-
-    def new_random_samples():
-        for h in hyperparameters:
+    def new_random_samples(self):
+        for h in self.hyperparameters:
             h.random_sample()
-        return tuple(h.value for h in hyperparameters)
+        return tuple(h.value for h in self.hyperparameters)
 
-    def new_product_samples(product):
+    def new_product_samples(self, product):
         for i, h_value in enumerate(product):
-            hyperparameters[i].value = h_value
-        return tuple(h.value for h in hyperparameters)
+            self.hyperparameters[i].value = h_value
+        return tuple(h.value for h in self.hyperparameters)
 
-    def write_history(filename, history):
-        if filename:
+    def create_history_file(self):
+        print("Preparing file where tuning results will be written ...")
+        h_names = ",".join(h.name for h in self.hyperparameters)
+        self.file_name = h_names + "@pid" + str(os.getpid()) + ".csv"
+
+        with open(tune_path + "/" + self.file_name, "w") as f:
+            f.write(h_names + "," + "MAP@10\n")
+
+    def write_history(self):
+        if self.file_name:
             print("Writing to file results ...")
-            with open(tune_path + "/" + filename, "a") as f:
-                for k, v in history.items():
+            with open(tune_path + "/" + self.file_name, "a") as f:
+                for k, v in self.history.items():
                     s_map = "{:.5f}".format(v)
                     f.write(",".join(str(el) for el in k) + "," + s_map)
                     f.write("\n")
 
-    assert len(hyperparameters) > 0, "You must specify the hyperparameters to search for"
+    def print_history(self):
+        pprint.pprint(self.history, width=1)
 
-    cartesian_products = None
-    if cartesian_product:
+    def run(self):
+        try:
+            while True:
+                # Sample new hyperparameters from their intervals for a new run
+                if self.cartesian_product:
 
-        if len(hyperparameters) == 1:
-            print("Cartesian product search needs atleast 2 hyperparameters!")
-            return
-
-        cartesian_products = list(itertools.product(*[h.interval for h in hyperparameters]))
-        shuffle(cartesian_products)
-
-    # Create file to write if write_to_file is set
-    file_name = None
-    if write_to_file:
-        print("Preparing file where tuning results will be written ...")
-        h_names = ",".join(h.name for h in hyperparameters)
-        file_name = h_names + "@pid" + str(os.getpid()) + ".csv"
-
-        with open(tune_path + "/" + file_name, "w") as f:
-            f.write(h_names + "," + "MAP@10\n")
-
-    # Dict to store the results obtained for different combination for hyperparameters
-    # Key -> Tuple(h1, h2, ..., hn) where hi is the ith hyperparameter value -> MAP@K
-    history = dict()
-    resample_try = 0
-
-    try:
-        while True:
-            # Sample new hyperparameters from their intervals for a new run
-            if cartesian_product:
-
-                # We checked all the possible values
-                if not cartesian_products:
-                    print("Exhausted all possible combinations, printing results ...")
-                    pprint.pprint(history, width=1)
-                    write_history(file_name, history)
-                    return
-
-                new_product = cartesian_products.pop()
-                new_hyperparameters = new_product_samples(new_product)
-
-            else:
-                # Randomly sample from the hyperparameters intervals
-                # If we can't find a new combination, just terminate
-                new_hyperparameters = new_random_samples()
-                while new_hyperparameters in history:
-                    # Try to find a combination not already evaluated
-                    resample_try += 1
-
-                    if resample_try == max_resample:
-                        # We probably exhausted all the combinations in the search space
-                        print("Interrupted tuning because of max resample, printing results ...")
-                        pprint.pprint(history, width=1)
-                        write_history(file_name, history)
+                    # We checked all the possible values
+                    if not self.cartesian_products:
+                        print("Exhausted all possible combinations, printing results ...")
+                        self.print_history()
+                        self.write_history()
                         return
 
-                    new_hyperparameters = new_random_samples()
+                    new_product = self.cartesian_products.pop()
+                    new_hyperparameters = self.new_product_samples(new_product)
 
-                resample_try = 0
+                else:
+                    # Randomly sample from the hyperparameters intervals
+                    # If we can't find a new combination, just terminate
+                    new_hyperparameters = self.new_random_samples()
+                    while new_hyperparameters in self.history:
+                        # Try to find a combination not already evaluated
+                        self.resample_try += 1
 
-            # Run evaluation and store the results in the history
-            start = timer()
-            print("Evaluating model with the following parameters")
-            o_string = "\n".join(h.name + ": " + str(h.value) for h in hyperparameters)
-            print(o_string)
-            map_at_k = parse_callable(callable_model).evaluate()
-            print("Evaluated model with the following parameters")
-            print(o_string)
-            print("Got MAP@10: {:.5f}\n".format(map_at_k))
-            print("elapsed: {:.3f}s\n".format(timer() - start))
-            history.update({new_hyperparameters: map_at_k})
+                        if self.resample_try == self.max_resample:
+                            # We probably exhausted all the combinations in the search space
+                            print("Interrupted tuning because of max resample, printing results ...")
+                            self.print_history()
+                            self.write_history()
+                            return
 
-    except KeyboardInterrupt:
-        print("Interrupted tuning, printing results ...")
-        pprint.pprint(history, width=1)
-        write_history(file_name, history)
+                        new_hyperparameters = self.new_random_samples()
+
+                    self.resample_try = 0
+
+                # Run evaluation and store the results in the history
+                start = timer()
+                print("Evaluating model with the following parameters")
+                o_string = "\n".join(h.name + ": " + str(h.value) for h in self.hyperparameters)
+                print(o_string)
+                map_at_k = self.parse_callable(self.callable_model).evaluate()
+                print("Evaluated model with the following parameters")
+                print(o_string)
+                print("Got MAP@10: {:.5f}\n".format(map_at_k))
+                print("elapsed: {:.3f}s\n".format(timer() - start))
+                self.history.update({new_hyperparameters: map_at_k})
+
+        except KeyboardInterrupt:
+            print("Interrupted tuning, printing results ...")
+            self.print_history()
+            self.write_history()
+
