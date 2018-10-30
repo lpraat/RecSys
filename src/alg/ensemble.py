lpraat/RecSys

commit 72d7f4a60a29447215be60853f96788d7e7877e0

Predit algorithm optimized for sparse matrices + Hybrid algorithm (ex. Ensamble) + Ensemble recsys (uniform combination of predictions)

src/alg/ensemble.py
@@ -2,8 +2,6 @@
 This file contains the Ensemble recommender which combines different models.
 """
 
-from timeit import default_timer as timer
-
 import numpy as np
 import scipy.sparse as sp
 from timeit import default_timer as timer
@@ -20,7 +18,7 @@ class Ensemble(RecSys):
     models according to a defined distribution.
     """
 
-    def __init__(self, *models):
+    def __init__(self, *models, method="combining"):
         """
         Constructor
         
@@ -35,13 +33,37 @@ class Ensemble(RecSys):
 
         # Initial values
         self.models = models
-    
+        self.method = method
 
-    def rate(self, dataset):
+    def run(self, dataset=None, targets=None, k=10):
 
-        # Compute combined ratings
-        ratings = sp.csr_matrix(dataset.shape, dtype=np.float32)
-        for model, w in self.models:
-            ratings += model.rate(dataset) * w
+        # Run each model
+        ensemble_preds = []
+        for model in self.models:
+            ensemble_preds.append(model.run(dataset=dataset, targets=targets, k=k))
+
+        if self.method == "combining":
+            n = len(self.models)
+            # Sanity check
+            assert n <= k, "too many models for 'combining' method (max. {})".format(k)
+
+            print("combining results ...")
+            start = timer()
+            # Combining list predictions
+            preds = []
+            for t in range(len(ensemble_preds[0])):
+                pred = []
+                i = 0
+                while i < k:
+                    # Get prediction from i-th model
+                    pred.append(ensemble_preds[i % n][t][1][i // n])
+                    i += 1
+
+                #print("{}, {}, {}".format(i, k, ensemble_preds[0][t][0]))
+                preds.append((ensemble_preds[0][t][0], pred))
+            print("elapsed time: {:.3f}\n".format(timer() - start))
+            
+            return preds
         
-        return ratings
+        else:
+            raise NotImplementedError("method '{}' not implemented".format(self.method))

src/alg/utils.py
@@ -226,31 +226,42 @@ def predict(ratings, targets=None, k=10, mask=None, invert_mask=False):
     # Convert to csr for fast row access
     mask = mask.tocsr()
     ratings = ratings.tocsr()
-
-    # Apply mask
-    if mask is not None and not invert_mask:
-        ratings = ratings.multiply(mask).tocsr()
     
     # Compute individually for each user
     preds = []
     for ui in targets:
         # Get row
-        ratings_i = ratings.getrow(ui).A.ravel()
+        ratings_i_start = ratings.indptr[ui]
+        ratings_i_end = ratings.indptr[ui + 1]
+        ratings_i_data = ratings.data[ratings_i_start:ratings_i_end]
+        ratings_i_indices = ratings.indices[ratings_i_start:ratings_i_end]
 
-        if mask is not None and invert_mask:
+        if mask is not None:
             # Apply inverted mask
-            ratings_i = ratings_i * (1 - mask.getrow(ui).A.ravel())
+            mask_i_start = mask.indptr[ui]
+            mask_i_end = mask.indptr[ui + 1]
+            mask_i_indices = mask.indices[mask_i_start:mask_i_end]
+
+            masked_i = np.in1d(ratings_i_indices, mask_i_indices, assume_unique=True, invert=invert_mask)
+            ratings_i_data = ratings_i_data[masked_i]
+            ratings_i_indices = ratings_i_indices[masked_i]
 
         # Compute top k items
         # Using argpartition the complexity is linear
-        # in the number of items if k << number of items
+        # in the number of non-zero items if k << number of nnz
         # ------------------------------------
-        # Complexity: O(len(items) + k log(k))
-        top_idxs = np.argpartition(ratings_i, -k)[-k:]
-        sort_idxs = np.argsort(-ratings_i[top_idxs])
+        # Complexity: O(len(nnz) + k log(k))
+        if len(ratings_i_data) > k:
+            top_idxs = np.argpartition(ratings_i_data, -k)[-k:]
+            ratings_i_indices = ratings_i_indices[top_idxs]
+            sort_idxs = np.argsort(-ratings_i_data[top_idxs])
+        else:
+            ratings_i_data.resize(k)
+            ratings_i_indices.resize(k)
+            sort_idxs = np.argsort(-ratings_i_data)
 
         # Add to list
-        preds.append((ui, list(top_idxs[sort_idxs])))
+        preds.append((ui, list(ratings_i_indices[sort_idxs])))
 
     # Return predictions
     return preds

src/alg/__init__.py
@@ -1,6 +1,7 @@
 # Export algorithms
 from .content_knn import ContentKNN
 from .ensemble import Ensemble
+from .hybrid import Hybrid
 from .item_knn import ItemKNN
 from .user_clusterize import UserClusterize
 from .user_knn import UserKNN

src/alg/hybrid.py
@@ -1,0 +1,45 @@
+"""
+This file contains the Ensemble recommender which combines different models.
+"""
+
+import numpy as np
+import scipy.sparse as sp
+from timeit import default_timer as timer
+
+from src.metrics import evaluate
+from .recsys import RecSys
+
+
+class Hybrid(RecSys):
+    """
+    Ensemble recommender.
+
+    Recommends items to users by combining ratings from different
+    models according to a defined distribution.
+    """
+
+    def __init__(self, *models):
+        """
+        Constructor
+        
+        Parameters
+        -----------
+        *models : RecSys
+            List of recommender system to combine when computing final ratings
+        """
+
+        # Super constructor
+        super().__init__()
+
+        # Initial values
+        self.models = models
+    
+
+    def rate(self, dataset):
+
+        # Compute combined ratings
+        ratings = sp.csr_matrix(dataset.shape, dtype=np.float32)
+        for model, w in self.models:
+            ratings += model.rate(dataset) * w
+        
+        return ratings

commit 78dbc66e428c4b815e85dab002c065e2a0ab2c8d

Fixed bug in src.alg.utils.predict introduced by commit 72d7f4a60a29447215be60853f96788d7e7877e0

src/alg/ensemble.py
@@ -54,12 +54,13 @@ class Ensemble(RecSys):
             for t in range(len(ensemble_preds[0])):
                 pred = []
                 i = 0
-                while i < k:
+                while len(pred) < k:
                     # Get prediction from i-th model
-                    pred.append(ensemble_preds[i % n][t][1][i // n])
+                    item = ensemble_preds[i % n][t][1][i // n]
+                    if item not in pred:
+                        pred.append(item)
                     i += 1
 
-                #print("{}, {}, {}".format(i, k, ensemble_preds[0][t][0]))
                 preds.append((ensemble_preds[0][t][0], pred))
             print("elapsed time: {:.3f}\n".format(timer() - start))
             

src/alg/user_knn.py
@@ -20,7 +20,6 @@ class UserKNN(RecSys):
     Recommends base on the similarity between users
     """
 
-
     def __init__(self, alpha=0.5, asym=True, knn=np.inf, h=0, qfunc=None):
         """
         Constructor
@@ -48,7 +47,6 @@ class UserKNN(RecSys):
         self.h = np.float32(h)
         self.qfunc = qfunc
         self.knn = knn
-    
     
     def rate(self, dataset):
 

src/alg/utils.py
@@ -230,7 +230,7 @@ def predict(ratings, targets=None, k=10, mask=None, invert_mask=False):
     # Compute individually for each user
     preds = []
     for ui in targets:
-        # Get row
+        # Get rows
         ratings_i_start = ratings.indptr[ui]
         ratings_i_end = ratings.indptr[ui + 1]
         ratings_i_data = ratings.data[ratings_i_start:ratings_i_end]
@@ -251,17 +251,20 @@ def predict(ratings, targets=None, k=10, mask=None, invert_mask=False):
         # in the number of non-zero items if k << number of nnz
         # ------------------------------------
         # Complexity: O(len(nnz) + k log(k))
-        if len(ratings_i_data) > k:
+        if len(ratings_i_indices) > k:
             top_idxs = np.argpartition(ratings_i_data, -k)[-k:]
-            ratings_i_indices = ratings_i_indices[top_idxs]
+            items_i = ratings_i_indices[top_idxs]
             sort_idxs = np.argsort(-ratings_i_data[top_idxs])
         else:
-            ratings_i_data.resize(k)
-            ratings_i_indices.resize(k)
-            sort_idxs = np.argsort(-ratings_i_data)
+            # @todo hardcoded, not very elegant but it works
+            top_pop = np.array([ 2272, 18266, 13980,  2674, 17239, 10496, 15578,  5606, 10848, 8956])
+            delta = k - len(ratings_i_indices)
+            items_i = np.append(ratings_i_indices, top_pop[np.in1d(top_pop, ratings_i_indices, assume_unique=True, invert=True)])
+            ratings_i = np.append(ratings_i_data, np.zeros(delta, dtype=np.float32))
+            sort_idxs = np.argsort(-ratings_i)
 
         # Add to list
-        preds.append((ui, list(ratings_i_indices[sort_idxs])))
+        preds.append((ui, list(np.resize(items_i[sort_idxs], k))))
 
     # Return predictions
     return preds

src/data.py
@@ -7,6 +7,7 @@ import math
 import os
 import pickle
 import random
+import numpy as np
 from timeit import default_timer as timer
 
 from src.const import cache_path, NUM_PLAYLIST
@@ -189,7 +190,8 @@ class Cache:
             "test_set": test_set,
             "album_set": album_set,
             "artist_set": artist_set,
-            "targets": targets
+            "targets": targets,
+            "top_pop": np.array([ 2272, 18266, 13980,  2674, 17239, 10496, 15578,  5606, 10848, 8956])
         })
 
     def fetch(self, key):