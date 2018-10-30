64878c79910b3ce6c6e5faa83ad577a722db14b6
Sneppy <andrea.mecchia@mail.polimi.it>
2018-10-30 23:46:34 +0100

    Added round-robin Ensemble

--- src/alg/ensemble.py
+++ src/alg/ensemble.py
@@ -37,34 +37,68 @@ class Ensemble(RecSys):
 
     def run(self, dataset=None, targets=None, k=10):
 
-        # Run each model
-        ensemble_preds = []
-        for model in self.models:
-            ensemble_preds.append(model.run(dataset=dataset, targets=targets, k=k))
-
         if self.method == "combining":
             n = len(self.models)
-            # Sanity check
-            assert n <= k, "too many models for 'combining' method (max. {})".format(k)
+            
+            # Run each model
+            ensemble_preds = []
+            for model in self.models:
+                ensemble_preds.append(model.run(dataset=dataset, targets=targets, k=k))
 
             print("combining results ...")
             start = timer()
             # Combining list predictions
             preds = []
-            for t in range(len(ensemble_preds[0])):
+            for ti in range(len(ensemble_preds[0])):
                 pred = []
                 i = 0
                 while len(pred) < k:
                     # Get prediction from i-th model
-                    item = ensemble_preds[i % n][t][1][i // n]
+                    item = ensemble_preds[i % n][ti][1][i // n]
                     if item not in pred:
                         pred.append(item)
                     i += 1
 
-                preds.append((ensemble_preds[0][t][0], pred))
+                preds.append((ensemble_preds[0][ti][0], pred))
             print("elapsed time: {:.3f}\n".format(timer() - start))
             
             return preds
         
+        elif self.method == "roundrobin":
+            n = len(self.models)
+            
+            # Run each model
+            ensemble_preds = []
+            for model, _ in self.models:
+                ensemble_preds.append(model.run(dataset=dataset, targets=targets, k=k))
+
+            # Normalize probabilities
+            cdf = np.array([m[1] for m in self.models])
+            cdf /= cdf.sum()
+
+            print("combining results with probabilities {} ...".format(cdf))
+            start = timer()
+            # Combine list predictions
+            preds = []
+            for ti in range(len(ensemble_preds[0])):
+                # Generate pick strategy with probabilities cdf
+                choices = np.random.choice(n, size=k, p=cdf)
+
+                pred = []
+                offset = [0 for _ in range(n)]
+                for mi in choices:
+                    # Ensure no duplicates
+                    item = ensemble_preds[mi][ti][1][offset[mi]]; offset[mi] += 1
+                    while item in pred:
+                        item = ensemble_preds[mi][ti][1][offset[mi]]; offset[mi] += 1
+                    
+                    # Append to predictions
+                    pred.append(item)
+                
+                preds.append((ensemble_preds[0][ti][0], pred))
+            print("elapsed time: {:.3f}\n".format(timer() - start))
+
+            return preds
+
         else:
             raise NotImplementedError("method '{}' not implemented".format(self.method))
