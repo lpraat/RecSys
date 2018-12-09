a78466757318c505ee72c3f8e885fc42d39a60d0
lpraat <pratissoli@gmail.com>
2018-12-09 13:42:59 +0100

    new model base

--- src/alg/smart_ensemble/initialize_sets.py
+++ src/alg/smart_ensemble/initialize_sets.py
@@ -5,10 +5,11 @@ import os
 
 import pickle
 
-from src.alg import ItemKNN
+from src.alg import ItemKNN, Hybrid, UserKNN
 from src.alg.hybrid_similarity import HybridSimilarity
 from src.alg.p3alpha import P3Alpha
 from src.alg.rp3beta import RP3Beta
+from src.alg.slim import Slim
 from src.alg.utils import predict
 from src.data import Cache, build_train_set_uniform
 from src.metrics import evaluate
@@ -75,17 +76,43 @@ def initialize_sets(k=5):
         with open(rel_path_sets + "/test.obj", "wb") as f:
             pickle.dump(test_set, f)
 
-        hybrid_graph = HybridSimilarity((P3Alpha(knn=1024, alpha=1), 0.15), (RP3Beta(knn=1024, alpha=1, beta=0.5), 0.95))
+        hybrid_graph = HybridSimilarity((P3Alpha(knn=1024, alpha=1), 0.15),
+                                        (RP3Beta(knn=1024, alpha=1, beta=0.5), 0.95))
         write_preds(path=rel_path_preds, name="hybrid_graph", model=hybrid_graph, dataset=train_set)
 
-        item_knn = ItemKNN()
-        write_preds(path=rel_path_preds, name="item_knn", model=item_knn, dataset=train_set)
-
+        print("map")
         preds = build_preds(rel_path_preds, "hybrid_graph")
         print(evaluate(preds, test_set))
 
-        preds = build_preds(rel_path_preds, "item_knn")
+        hybrid_item_user = Hybrid((ItemKNN(("artist_set", 0.1, {}), ("album_set", 0.2, {})), 0.3),
+                                  (UserKNN(knn=200), 0.2))
+        write_preds(path=rel_path_preds, name="hybrid_item_user", model=hybrid_item_user, dataset=train_set)
+
+        print("map")
+        preds = build_preds(rel_path_preds, "hybrid_item_user")
         print(evaluate(preds, test_set))
 
+        double_slim = HybridSimilarity((Slim(epochs=1, lr=0.1, lambda_i=0.01, all_dataset=False), 0.5),
+                                       (Slim(epochs=1, lr=0.1, lambda_j=0.01, all_dataset=False), 0.5))
+        write_preds(path=rel_path_preds, name="double_slim", model=double_slim, dataset=train_set)
 
+        print("map")
+        preds = build_preds(rel_path_preds, "double_slim")
+        print(evaluate(preds, test_set))
+
+        slim = Slim(epochs=3, lr=0.1, lambda_i=0.025, lambda_j=0.025, all_dataset=False)
+        write_preds(path=rel_path_preds, name="slim", model=slim, dataset=train_set)
+
+        print("map")
+        preds = build_preds(rel_path_preds, "slim")
+        print(evaluate(preds, test_set))
+
+        h1 = HybridSimilarity((ItemKNN(("artist_set", 0.1, {}), ("album_set", 0.2, {})), 0.7),
+                              (Slim(lambda_i=0.025, lambda_j=0.025, all_dataset=False, epochs=3, lr=0.1), 0.3))
+        m1 = Hybrid((h1, 0.85), (UserKNN(knn=190), 0.15))
+        write_preds(path=rel_path_preds, name="forzajuve", model=m1, dataset=train_set)
+
+        print("map")
+        preds = build_preds(rel_path_preds, "forzajuve")
+        print(evaluate(preds, test_set))
 
